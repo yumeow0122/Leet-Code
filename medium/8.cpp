aa5b22995bf2ab9acc16237918aa907522dcf390
yumeow0122 <yumeow0122@gmail.com>
2022-12-09 21:51:07 +0800

    fix 8

--- medium/8.cpp
+++ medium/8.cpp
@@ -2,28 +2,33 @@
 
 using namespace std;
 
-class Solution {
+class Solution
+{
 public:
-int myAtoi(string str) {
-    long result = 0;
-    int indicator = 1;
-    for(int i = 0; i<str.size();)
+    int myAtoi(string str)
     {
-        i = str.find_first_not_of(' ');
-        if(str[i] == '-' || str[i] == '+')
-            indicator = (str[i++] == '-')? -1 : 1;
-        while('0'<= str[i] && str[i] <= '9') 
-        {
-            result = result*10 + (str[i++]-'0');
-            if(result*indicator >= INT_MAX) return INT_MAX;
-            if(result*indicator <= INT_MIN) return INT_MIN;                
-        }
-        return result*indicator;
-    }
-}
-};
 
-int main(){
+        long result = 0;
+        int indicator = 1;
+
+        int i = str.find_first_not_of(' ');
+        if (i == -1)
+            return 0;
+        if (str[i] == '-' || str[i] == '+')
+            indicator = (str[i++] == '-') ? -1 : 1;
+        while ('0' <= str[i] && str[i] <= '9')
+        {
+            result = result * 10 + (str[i++] - '0');
+            if (result * indicator >= INT_MAX)
+                return INT_MAX;
+            if (result * indicator <= INT_MIN)
+                return INT_MIN;
+        }
+        return result * indicator;
+    }
+};
+int main()
+{
     string str = "123";
 
     Solution solution;
