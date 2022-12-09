467b4bb99ab9ec4e5c10955211d3c6ec2e57214a
yumeow0122 <yumeow0122@gmail.com>
2022-12-09 21:39:28 +0800

    20221209: * 8. String to Integer (atoi)

---
+++ medium/8.cpp
@@ -0,0 +1,34 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+class Solution {
+public:
+int myAtoi(string str) {
+    long result = 0;
+    int indicator = 1;
+    for(int i = 0; i<str.size();)
+    {
+        i = str.find_first_not_of(' ');
+        if(str[i] == '-' || str[i] == '+')
+            indicator = (str[i++] == '-')? -1 : 1;
+        while('0'<= str[i] && str[i] <= '9') 
+        {
+            result = result*10 + (str[i++]-'0');
+            if(result*indicator >= INT_MAX) return INT_MAX;
+            if(result*indicator <= INT_MIN) return INT_MIN;                
+        }
+        return result*indicator;
+    }
+}
+};
+
+int main(){
+    string str = "123";
+
+    Solution solution;
+
+    cout << solution.myAtoi(str) << endl;
+
+    return 0;
+}
